13c7c69dd2ef393a8d9e149687ef102e21d433a7
JustDams <ddw_github@de-weerd.name>
2021-02-22 18:50:43 +0100

    :sparkles: feat router & upload form

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import UploadImage from './UploadImage.vue'
 
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
+const routes = {
+  '/': App,
+  '/UploadImage': UploadImage,
+}
+
 new Vue({
-  render: h => h(App),
+  render: h => h(routes[window.location.pathname]),
 }).$mount('#app')

4043dc6a34fa35990463cc249c9c69f703b44fc1
EREN <erik.ren@ynov.com>
2021-02-22 19:46:59 +0100

    :sparkles: feat page Error

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import Error from './Error.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(Error),
 }).$mount('#app')

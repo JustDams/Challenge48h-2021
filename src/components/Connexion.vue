2dd8069bebd6a16bcc4acea31b853fe0377defe0
GAIO DOS SANTOS Lucas <lucas.gaildossantos@gmail.com>
2021-02-23 08:20:59 +0100

    :construction: add html structure

--- src/components/Connexion.vue
+++ src/components/Connexion.vue
@@ -1,5 +1,11 @@
 <template>
-
+    <main>  
+        <form action="">
+            <input type="email" name="" id="">
+            <input type="password" name="" id="">
+            <input type="submit" value="">
+        </form>
+    </main>
 </template>
 
 <script>

fedcae2d39211434232eccb5fa6e100bdeb68973
JustDams <ddw_github@de-weerd.name>
2021-02-23 11:39:09 +0100

    :sparkles: feat image & dist

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+  <div id="app" class="container">
+    <div class="d-flex flex-wrap justify-content-center">
+      <ImageComponent
+        :imageId="i.id"
+        v-for="i in images"
+        :key="i.image[0].id"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import ImageComponent from "./components/ImageComponent.vue";
+import axios from "axios";
+import { apiUrl } from "../config.json";
 export default {
   name: "App",
+  components: {
+    ImageComponent,
+  },
+  data() {
+    return {
+      images: null,
+    };
+  },
+  mounted() {
+    axios.get(`${apiUrl}/images`).then((res) => {
+      this.images = res.data;
+    });
+  },
 };
 </script>
 
